392a593d11f767076edd66396881892e095b81d7
Ben Chao <88502044+somerabbit@users.noreply.github.com>
2023-05-12 13:32:59 -0700

    small change to Organization

--- java/com/aa/act/interview/org/Organization.java
+++ java/com/aa/act/interview/org/Organization.java
@@ -19,8 +19,31 @@ public abstract class Organization {
 	 * @param title
 	 * @return the newly filled position or empty if no position has that title
 	 */
+	int id = 0;
 	public Optional<Position> hire(Name person, String title) {
 		//your code here
+		Optional<Position> optionalPosition = findPositionByTitle(root,title);
+		if (optionalPosition.isPresent()){
+			Position position = optionalPosition.get();
+			if (!position.isFilled()){
+				Employee employee = new Employee(id++,person);
+				position.setEmployee(Optional.of(employee));
+				return Optional.of(position);
+			}
+		}
+		return Optional.empty();
+	}
+
+	public Optional<Position> findPositionByTitle(Position current, String title){
+		if (current.getTitle().equals(title)){
+			return Optional.of(current);
+		}
+		for (Position directReport : current.getDirectReports()){
+			Optional<Position> result = findPositionByTitle(directReport,title);
+			if (result.isPresent()){
+				return result;
+			}
+		}
 		return Optional.empty();
 	}
 
